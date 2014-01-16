3f1a64433eb820a508a4770c3f9fb87a010a87ef
Will Franklin <will.franklin@guardian.co.uk>
2014-01-16 17:48:05 +0000

    Use colours from json file

--- chart/handlers/pie.py
+++ chart/handlers/pie.py
@@ -2,6 +2,9 @@ import pygal
 from pygal.style import LightStyle
 
 def render(chart):
+    style = LightStyle
+    style.colors = tuple(row['colour'] for row in chart['rows'])
+    style.plot_background = style.background
     pie = pygal.Pie(style=LightStyle)
     for row in chart['rows']:
         pie.add(row['label'], row['value'])
