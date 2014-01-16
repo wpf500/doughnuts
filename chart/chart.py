a5bde61965b6d6e72e6ad955c7bb1e578b92bcbb
Will Franklin <will.franklin@guardian.co.uk>
2014-01-16 22:15:10 +0000

    Start a custom style

--- chart/chart.py
+++ chart/chart.py
@@ -1,4 +1,18 @@
 #!/usr/bin/python
+from pygal.style import Style
+
+# create a new Style each time because we can overwrite colours
+def style():
+    return Style(
+        background='white',
+        plot_background='white',
+        foreground='rgba(0, 0, 0, 0.7)',
+        foreground_light='rgba(0, 0, 0, 0.9)',
+        foreground_dark='rgba(0, 0, 0, 0.5)',
+        colors=('#242424', '#9f6767', '#92ac68',
+                '#d0d293', '#9aacc3', '#bb77a4',
+                '#77bbb5', '#777777'))
+
 def render(chart_data):
     module = __import__('handlers.%s' % chart_data['type'], fromlist=['render'])
 
