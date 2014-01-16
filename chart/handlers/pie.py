e797341d75b3b1384182035146fa374a9c31c9e8
Will Franklin <will.franklin@guardian.co.uk>
2014-01-16 22:55:50 +0000

    Work on appearence

--- chart/handlers/pie.py
+++ chart/handlers/pie.py
@@ -7,7 +7,10 @@ def render(chart, style, inner_radius=0):
     style.colors = [row.get('colour', alt) for row, alt in
             izip(rows, cycle(style.colors))]
 
-    pie = pygal.Pie(style=style, inner_radius=inner_radius)
+    pie = pygal.Pie(style=style, js=[],
+            inner_radius=inner_radius,
+            value_font_size=14,
+            font_family='arial')
     for row in rows:
         pie.add(row['label'], row['value'])
     return pie
