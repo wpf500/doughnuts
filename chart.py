935e37bad6320765ea2f491aa0fb8fd162e3c32f
Will Franklin <will.franklin@guardian.co.uk>
2014-01-16 17:20:16 +0000

    Add source

--- chart.py
+++ chart.py
@@ -6,4 +6,5 @@ module = __import__('handlers.%s' % chart_data['type'], fromlist=['render'])
 
 chart = module.render(chart_data)
 chart.title = chart_data['title']
+chart.add_source(chart_data.get('source'))
 print chart.render()
