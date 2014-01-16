6e3a0d7b840ec2ef73f6a97b44705222a813aed7
Will Franklin <will.franklin@guardian.co.uk>
2014-01-16 16:12:44 +0000

    Generate our own pie chart

--- chart.py
+++ chart.py
@@ -5,10 +5,10 @@ chart_data = json.loads(sys.stdin.read())
 
 module = __import__('handlers.%s' % chart_data['type'], fromlist=['render'])
 
-print '<svg xmlns="http://www.w3.org/2000/svg">'
-print '<g transform="translate(400, 300)">'
+print '<html>'
+print '<head><link rel="stylesheet" href="style.css" /></head>'
+print '<body><svg xmlns="http://www.w3.org/2000/svg">'
 
 module.render(chart_data)
 
-print '</g>'
-print '</svg>'
+print '</svg></body></html>'
--- handlers/doughnut.py
+++ handlers/doughnut.py
@@ -1,26 +1,39 @@
 import math
 
 radius = 100
+text_radius = 120
+d_radius = 40
+
+def polar2cart(r, angle):
+    return [r * -math.sin(angle), r * math.cos(angle)]
 
 def render(chart_data):
     data = chart_data['data']
 
+    print '<g transform="translate(%d, %d)">' % (radius, radius)
+
     total = sum(float(row['value']) for row in data)
     angles = [2 * math.pi * float(row['value']) / total for row in data]
 
-    x1 = 0
-    y1 = radius
+    arc_x1 = 0
+    arc_y1 = radius
+    arc_angle = 2 * math.pi
 
-    angle_left = 2 * math.pi
+    outlines = ''
 
     for angle, row in zip(angles, data):
-        angle_left -= angle;
+        text_angle = arc_angle - angle / 2
+        text_x, text_y = polar2cart(text_radius, text_angle)
+        print '<text class="doughnut-label" x="%f" y="%f">%s</text>' % (text_x, text_y, row['label'])
 
-        x2 = radius * -math.sin(angle_left)
-        y2 = radius * math.cos(angle_left)
-        a = 1 if angle > math.pi else 0
+        arc_angle -= angle;
+        arc_x2, arc_y2 = polar2cart(radius, arc_angle)
+        arc_cw = 1 if angle > math.pi else 0
+        print '<path class="doughnut-arc" fill="%s" d="M 0 0 L %f %f A %d %d 0 %d 0 %f %f" />' % (row['color'], arc_x1, arc_y1, radius, radius, arc_cw, arc_x2, arc_y2)
 
-        print '<path fill="%s" d="M 0 0 L %f %f A %d %d 0 %d 0 %f %f" />' % (row['color'], x1, y1, radius, radius, a, x2, y2)
+        arc_x1 = arc_x2
+        arc_y1 = arc_y2
 
-        x1 = x2
-        y1 = y2
+    print '<circle cx="0" cy="0" r="%d" fill="white" />' % d_radius
+    print '<text class="doughnut-label" x="0" y="0" alignment-baseline="middle" text-anchor="middle">%s</text>' % chart_data['label_middle']
+    print '</g>'
