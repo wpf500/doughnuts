fdac167d064c9ec73305b441a93bc511e45c75ed
Will Franklin <will.franklin@guardian.co.uk>
2014-01-17 12:27:12 +0000

    You can optionally provide y labels

--- chart/handlers/line.py
+++ chart/handlers/line.py
@@ -6,9 +6,11 @@ def render(chart_data):
 
     config = chart.gu_config()
     config.css.append('style/line.css')
+    config.height = 450
 
     line = pygal.Line(style=chart.gu_style(rows), config=config)
     line.x_labels = chart_data['x_labels']
+    line.y_labels = chart_data.get('y_labels')
     for row in rows:
         line.add(row['label'], row['value'])
     return line
