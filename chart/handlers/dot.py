cec6fecd0332c2b8ea8c4d924617164aa14f9c72
Will Franklin <will.franklin@guardian.co.uk>
2014-01-17 14:07:01 +0000

    Added dot type

---
+++ chart/handlers/dot.py
@@ -0,0 +1,15 @@
+import pygal
+import chart
+
+def render(chart_data):
+    rows = chart_data['rows']
+
+    config = chart.gu_config()
+    config.print_values = False
+
+    radar = pygal.Dot(style=chart.gu_style(rows), config=config)
+    radar.x_labels = chart_data['x_labels']
+    radar.y_labels = chart_data.get('y_labels')
+    for row in rows:
+        radar.add(row['label'], row['value'])
+    return radar
